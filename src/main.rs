1fe226c03f90c9f6eca947bb0eba41504b1e5a7e
Nitori <nitori@chireiden.net>
2024-03-31 17:29:42 +0200

    Add cli output of emotes by highlighting them with ansi colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use irc::client::Client;
-use crate::irc::protocol::Command;
+use crate::irc::protocol::{Command, RichText};
 
 mod irc;
 
@@ -21,7 +21,24 @@ fn main() {
                 client.send_line(&format!("JOIN {}", channel)).unwrap();
             }
             Command::Privmsg if msg.is_channel_message() => {
-                println!("{} <{}> {}", msg.params[0], msg.display_name().unwrap(), msg.params[1]);
+                if let Ok(emotes) = msg.emotes() {
+                    let text_items = emotes.iter().map(|e| {
+                        match e {
+                            RichText::Text(s) => s.into(),
+                            RichText::Emote(e) => {
+                                let mut s = String::new();
+                                s.push_str("\x1b[32m[");
+                                s.push_str(&e.emote);
+                                s.push_str("]\x1b[0m");
+                                s
+                            }
+                        }
+                    }).collect::<Vec<String>>();
+                    let text_message = text_items.join("");
+                    println!("{} <{}> {}", msg.params[0], msg.display_name().unwrap(), text_message);
+                } else {
+                    println!("{} <{}> {}", msg.params[0], msg.display_name().unwrap(), msg.params[1]);
+                }
             }
             Command::Privmsg if msg.is_private_message() => {
                 println!("(private) <{}> {}", msg.display_name().unwrap(), msg.params[1]);
