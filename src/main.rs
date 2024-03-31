76d50e74bca756dd2787f578b40ec172761910a8
Nitori <nitori@chireiden.net>
2024-03-31 01:45:33 +0100

    Move nick and channel to environment and use better event for detecting join

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ mod irc;
 fn main() {
     dotenv::dotenv().ok();
     let token = std::env::var("OAUTH_TOKEN").expect("OAUTH_TOKEN is missing!");
-    let mut client = Client::new(&token, &"SaniSensei");
+    let nickname = std::env::var("NICKNAME").expect("NICKNAME is missing!");
+    let mut channel = std::env::var("CHANNEL").expect("CHANNEL is missing!");
+    if !channel.starts_with("#") {
+        channel = format!("#{}", channel);
+    }
+
+    let mut client = Client::new(&token, &nickname);
     client.connect();
 
     for msg in client.iter() {
         match msg.command {
             Command::Ready => {
-                client.send_line("JOIN #bloodstainedvt").unwrap();
+                client.send_line(&format!("JOIN {}", channel)).unwrap();
             }
             Command::Privmsg if msg.is_channel_message() => {
                 println!("{} <{}> {}", msg.params[0], msg.display_name().unwrap(), msg.params[1]);
@@ -20,8 +26,8 @@ fn main() {
             Command::Privmsg if msg.is_private_message() => {
                 println!("(private) <{}> {}", msg.display_name().unwrap(), msg.params[1]);
             }
-            Command::Names => {
-                println!("Joined: {}", msg.params[2]);
+            Command::EndOfNames => {
+                println!("Joined: {}", msg.params[1]);
             }
             _ => ()
         }
